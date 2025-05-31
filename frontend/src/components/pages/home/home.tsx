2aaeb46457cb0b007adbe167007540c543df7da1
Nikita Bondarev <bondarev.n17@yandex.ru>
2025-05-31 19:34:43 +0300

    Add zalupa

--- frontend/src/components/pages/home/home.tsx
+++ frontend/src/components/pages/home/home.tsx
@@ -1,12 +1,15 @@
 import { Container } from '../../container/container';
 import { Layout } from '../../layout/layout';
 import { Greeting } from '../../base-screen/greetings/greeting';
+import {Cards} from "../../base-screen/cards/cards";
 
 export const Home = () => {
 	return (
 		<Container>
 			<Layout>
 				<Greeting />
+
+				<Cards />
 			</Layout>
 		</Container>
 	);
