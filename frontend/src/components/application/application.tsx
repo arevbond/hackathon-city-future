24e613ebd80ff607c57090774a691c6ef8f0d5c3
crocodileDaniil <karasev_dan_an@mail.ru>
2025-06-01 15:50:53 +0300

    add button

--- frontend/src/components/application/application.tsx
+++ frontend/src/components/application/application.tsx
@@ -1,13 +1,25 @@
 import { ApplicationHeader } from './header/application-header';
 import { Form1 } from './application-form/form-1/form-1';
 import { Form2 } from './application-form/form-2/form2';
+import { useNavigate } from 'react-router-dom';
+import { pathPages } from '@utils/page-paths';
+import styles from './styles.module.css';
 
 export const Application = () => {
+	const navigate = useNavigate();
+
+	const onNavigate = () => {
+		navigate(pathPages.home, { replace: true });
+	};
+
 	return (
 		<div>
 			<ApplicationHeader />
 			<Form1 />
 			<Form2 />
+			<button onClick={onNavigate} className={styles['beautiful-button']}>
+				Отправить
+			</button>
 		</div>
 	);
 };
